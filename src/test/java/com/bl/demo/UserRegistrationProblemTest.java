6515a3561071030e5b2013e13f50624d6533ec85
Tushar-W <twankhede95@gmail.com>
2020-04-21 00:01:01 +0530

    ADDED:tested valid last name

--- src/test/java/com/bl/demo/UserRegistrationProblemTest.java
+++ src/test/java/com/bl/demo/UserRegistrationProblemTest.java
@@ -1,18 +1,25 @@
 package com.bl.demo;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class UserRegistrationProblemTest {
     @Test
-    public void givenName_WhenValid_ShouldReturnTrue() {
+    public void givenFirstName_WhenValid_ShouldReturnTrue() {
         UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
         Assert.assertTrue(userRegistrationProblem.checkValidateName("Tushar"));
     }
 
     @Test
-    public void givenName_WhenShort_ShouldReturnFalse() {
+    public void givenFirstName_WhenShort_ShouldReturnFalse() {
         UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
         Assert.assertFalse(userRegistrationProblem.checkValidateName("Tu"));
+    }
+
+    @Test
+    public void givenLastName_WhenValid_ShouldReturnTrue() {
+        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
+        Assert.assertTrue(userRegistrationProblem.checkValidateName("Wankhede"));
     }
 }
