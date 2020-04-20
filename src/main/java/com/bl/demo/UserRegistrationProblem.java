6059bcf6766e5d07925f11259cab8cb372e32c85
Tushar-W <twankhede95@gmail.com>
2020-04-21 00:10:25 +0530

    ADDED:function for check last name validate or not

--- src/main/java/com/bl/demo/UserRegistrationProblem.java
+++ src/main/java/com/bl/demo/UserRegistrationProblem.java
@@ -3,9 +3,9 @@ package com.bl.demo;
 import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
-    public final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{3,}$";
+    public final String NAME_PATTERN = "^[A-Z]{1}[a-z]{3,}$";
 
     public boolean checkValidateName(String name) {
-        return Pattern.matches(FIRST_NAME_PATTERN,name);
+        return Pattern.matches(NAME_PATTERN,name);
     }
 }
