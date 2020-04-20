c7a64c2ab25be0e8665a195ec55abf6aa3d2c432
Tushar-W <twankhede95@gmail.com>
2020-04-21 00:44:54 +0530

    ADDED:function for check email validate or not

--- src/main/java/com/bl/demo/UserRegistrationProblem.java
+++ src/main/java/com/bl/demo/UserRegistrationProblem.java
@@ -4,8 +4,13 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
     public final String NAME_PATTERN = "^[A-Z]{1}[a-z]{3,}$";
+    public final String EMAIL_PATTERN = "^[0-9a-zA-Z]{1,}([._+-]{1}[a-zA-Z]+)?[@][0-9a-zA-Z]{1,}[.][a-z]{2,4}([.]{1}[a-z]{2})?$";
 
     public boolean checkValidateName(String name) {
         return Pattern.matches(NAME_PATTERN,name);
+    }
+
+    public boolean checkValidateEmail(String email) {
+        return Pattern.matches(EMAIL_PATTERN,email);
     }
 }
