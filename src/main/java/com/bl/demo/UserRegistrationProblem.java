83a927d96a09134d2fef7b3e74be066eb12a22eb
Tushar-W <twankhede95@gmail.com>
2020-04-21 11:55:57 +0530

    REFACTORED:merge password all rules in once

--- src/main/java/com/bl/demo/UserRegistrationProblem.java
+++ src/main/java/com/bl/demo/UserRegistrationProblem.java
@@ -6,10 +6,7 @@ public class UserRegistrationProblem {
     public final String NAME_PATTERN = "^[A-Z]{1}[a-z]{3,}$";
     public final String EMAIL_PATTERN = "^[0-9a-zA-Z]{1,}([._+-]{1}[a-zA-Z]+)?[@][0-9a-zA-Z]{1,}[.][a-z]{2,4}([.]{1}[a-z]{2})?$";
     public final String MOBILE_NO_PATTERN="^[0-9]{1,3}[ ][6-9]{1}[0-9]{9}$";
-    public final String PASSWORD_PATTERN = "^[a-zA-Z0-9]{8,}$";
-    public final String PASSWORD_PATTERN1 = "^(?=.*[A-Z])[a-zA-Z0-9]{8,}$";
-    public final String PASSWORD_PATTERN2 = "^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$";
-    public final String PASSWORD_PATTERN3 = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-zA-Z0-9@#!$%^&*_-]{8,})[a-zA-Z0-9]{1,}[@#!$%^&*_-]{1}[a-zA-Z0-9]{1,}$";
+    public final String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-zA-Z0-9@#!$%^&*_-]{8,})[a-zA-Z0-9]{1,}[@#!$%^&*_-]{1}[a-zA-Z0-9]{1,}$";
 
     public boolean checkValidateName(String name) {
         return Pattern.matches(NAME_PATTERN,name);
@@ -25,17 +22,5 @@ public class UserRegistrationProblem {
 
     public boolean checkValidatePassword(String password) {
         return Pattern.matches(PASSWORD_PATTERN,password);
-    }
-
-    public boolean checkValidatePassword1(String password) {
-        return Pattern.matches(PASSWORD_PATTERN1,password);
-    }
-
-    public boolean checkValidatePassword2(String password) {
-        return Pattern.matches(PASSWORD_PATTERN2,password);
-    }
-
-    public boolean checkValidatePassword3(String password) {
-        return Pattern.matches(PASSWORD_PATTERN3,password);
     }
 }
--- src/test/java/com/bl/demo/UserRegistrationProblemTest.java
+++ src/test/java/com/bl/demo/UserRegistrationProblemTest.java
@@ -4,99 +4,55 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegistrationProblemTest {
+    UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
+
     @Test
     public void givenFirstName_WhenValid_ShouldReturnTrue() {
-        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
         Assert.assertTrue(userRegistrationProblem.checkValidateName("Tushar"));
     }
 
     @Test
     public void givenFirstName_WhenShort_ShouldReturnFalse() {
-        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
         Assert.assertFalse(userRegistrationProblem.checkValidateName("Tu"));
     }
 
     @Test
     public void givenLastName_WhenValid_ShouldReturnTrue() {
-        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
         Assert.assertTrue(userRegistrationProblem.checkValidateName("Wankhede"));
     }
 
     @Test
     public void givenLastName_WhenShort_ShouldReturnFalse() {
-        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
         Assert.assertFalse(userRegistrationProblem.checkValidateName("Wa"));
     }
 
     @Test
     public void givenEmail_WhenValid_ShouldReturnTrue() {
-        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
         Assert.assertTrue(userRegistrationProblem.checkValidateEmail("abc.xyz@bl.co.in"));
     }
 
     @Test
     public void givenEmail_WhenInvalid_ShouldReturnFalse() {
-        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
         Assert.assertFalse(userRegistrationProblem.checkValidateEmail("a@.com"));
     }
 
     @Test
     public void givenMobileNumber_WhenValid_ShouldReturnTrue() {
-        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
         Assert.assertTrue(userRegistrationProblem.checkValidateMobileNumber("91 9561782554"));
     }
 
     @Test
     public void givenMobileNumber_WhenInvalid_ShouldReturnFalse() {
-        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
         Assert.assertFalse(userRegistrationProblem.checkValidateMobileNumber("9988776556"));
     }
 
     @Test
     public void givenPassword_WhenValid_ShouldReturnTrue() {
-        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
-        Assert.assertTrue(userRegistrationProblem.checkValidatePassword("123abc45ff"));
+        Assert.assertTrue(userRegistrationProblem.checkValidatePassword("tushar@95Barca"));
     }
 
     @Test
     public void givenPassword_WhenInvalid_ShouldReturnFalse() {
-        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
-        Assert.assertFalse(userRegistrationProblem.checkValidatePassword("123abc@45ff"));
-    }
-
-    @Test
-    public void givenPassword_WhenAtLeastOneUpperCase_ShouldReturnTrue() {
-        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
-        Assert.assertFalse(userRegistrationProblem.checkValidatePassword1("Abc45f"));
-    }
-
-    @Test
-    public void givenPassword_WhenNotAtLeastOneUpperCase_ShouldReturnFalse() {
-        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
-        Assert.assertFalse(userRegistrationProblem.checkValidatePassword1("abc45ff"));
-    }
-
-    @Test
-    public void givenPassword_WhenAtLeastOneNumericNumber_ShouldReturnTrue() {
-        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
-        Assert.assertTrue(userRegistrationProblem.checkValidatePassword2("Abc456ff"));
-    }
-
-    @Test
-    public void givenPassword_WhenNotAtLeastOneNumericNumber_ShouldReturnFalse() {
-        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
-        Assert.assertFalse(userRegistrationProblem.checkValidatePassword2("Abcff"));
-    }
-
-    @Test
-    public void givenPassword_WhenExactlyOneSpecialCharacter_ShouldReturnTrue() {
-        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
-        Assert.assertTrue(userRegistrationProblem.checkValidatePassword3("tushar@95Barca"));
-    }
-
-    @Test
-    public void givenPassword_WhenNotExactlyOneSpecialCharacter_ShouldReturnFalse() {
-        UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
-        Assert.assertFalse(userRegistrationProblem.checkValidatePassword3("tushar95Barca"));
+        Assert.assertFalse(userRegistrationProblem.checkValidatePassword("tushar95Barca"));
     }
 }
