d24720cc34c3ee0497ab8f3c64a268ca3c003ca7
Tushar-W <twankhede95@gmail.com>
2020-04-21 09:45:33 +0530

    MODIFIED:create seperate function for password has at least one uppercase

--- src/main/java/com/bl/demo/UserRegistrationProblem.java
+++ src/main/java/com/bl/demo/UserRegistrationProblem.java
@@ -6,7 +6,7 @@ public class UserRegistrationProblem {
     public final String NAME_PATTERN = "^[A-Z]{1}[a-z]{3,}$";
     public final String EMAIL_PATTERN = "^[0-9a-zA-Z]{1,}([._+-]{1}[a-zA-Z]+)?[@][0-9a-zA-Z]{1,}[.][a-z]{2,4}([.]{1}[a-z]{2})?$";
     public final String MOBILE_NO_PATTERN="^[0-9]{1,3}[ ][6-9]{1}[0-9]{9}$";
-    //public final String PASSWORD_PATTERN = "^[a-zA-Z0-9]{8,}$";
+    public final String PASSWORD_PATTERN = "^[a-zA-Z0-9]{8,}$";
     public final String PASSWORD_PATTERN1 = "^(?=.*[A-Z])[a-zA-Z0-9]{8,}$";
 
     public boolean checkValidateName(String name) {
@@ -22,6 +22,10 @@ public class UserRegistrationProblem {
     }
 
     public boolean checkValidatePassword(String password) {
+        return Pattern.matches(PASSWORD_PATTERN,password);
+    }
+
+    public boolean checkValidatePassword1(String password) {
         return Pattern.matches(PASSWORD_PATTERN1,password);
     }
 }
